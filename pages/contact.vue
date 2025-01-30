fe160afd1be05f720d65214a48d8abc8569351fb
Vincent Flebus <vflebus@gmail.com>
2025-01-30 20:41:38 +0100

    contact form subject

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,7 +5,7 @@
       <h1 class="text-2xl lg:text-[64px] lg:leading-[64px] font-bold lg:font-normal font-arkhip uppercase text-center">Contact</h1>
       <p class="lg:text-lg text-justify">Vous avez une demande particulière ? Besoin d'une information ? Nous serons heureux de répondre à vos questions.</p>
       <h2 class="text-xl lg:text-3xl font-bold lg:font-normal font-arkhip uppercase mt-8">Contactez nous</h2>
-      <EnquiryForm subject="plan sur mesure" />
+      <EnquiryForm subject="autre" />
     </div>
     <img draggable="false" src="/images/misc/bras.webp" class="w-full h-[20vh] lg:w-[unset] lg:h-[unset] lg:grow min-w-0 object-cover" />
   </div>
